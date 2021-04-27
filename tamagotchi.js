ecf9e1a011f63d0bdd1cc6226454ec8f8187e88c
bannholtzer jonathan <bannholtzer.j@gmail.com>
2021-04-27 09:32:15 +0200

    update script

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,87 +1,80 @@
-//////// VARIABLE ///////
+//Script Tamaotchi
+
+// Variable
 var hungry = 1;
 var joy = 5;
 var energy = 10;
 
-//////// FONCTION ///////
-function manger(){
-    if (hungry >= 0 && joy >= 0 ) {
-        hungry -= 1;
-        joy = joy - 1;
-        energy += 2;
-    }
-    else{
-        console.log("Impossible.")
-    }    
+// Fonction
+function eat() {
+  if (hungry >= 0 && joy >= 0) {
+    hungry -= 1;
+    joy = joy - 1;
+    energy += 2;
+  } else {
+    console.log("Impossible.");
+  }
 }
 
-function jouer(faim,joie, energie){
-    if(energy >= 0 && hungry >= 0 ){
-        hungry +=  1;
-        joy +=  1;
-        energy -=  2;
-    }
-    else{
-        console.log("Impossible.")
-    }
+function play(faim, joie, energie) {
+  if (energy >= 0 && hungry >= 0) {
+    hungry += 1;
+    joy += 1;
+    energy -= 2;
+  } else {
+    console.log("Impossible.");
+  }
 }
 
-function afficher(){
-    elmHungry.innerText = hungry;
-    elmJoy.innerText = joy;
-    elmEnergy.innerText = energy;
+function score() {
+  elmHungry.innerText = hungry;
+  elmJoy.innerText = joy;
+  elmEnergy.innerText = energy;
 }
 
-////// regle /////
+//Règles de jeux
 
-function regle(){
-    if(joy < 0 || hungry > 5 || energy < 0){
-        const ban = document.createElement('div');
-     ban.innerText = "DEAD";
-     const status = document.getElementById('status');
-     status.appendChild(ban);
-    }
+function rules() {
+  if (joy < 0 || hungry > 5 || energy < 0) {
+    const ban = document.createElement("div");
+    ban.innerText = "DEAD";
+    const status = document.getElementById("status");
+    status.appendChild(ban);
+  }
 }
 
-//////// SELECTION ELEMENTS ///////
+//Sélection des éléments
 var game = document.getElementById("play");
 var eating = document.getElementById("eat");
-
 var elmHungry = document.getElementById("fm");
 var elmJoy = document.getElementById("happy");
 var elmEnergy = document.getElementById("eng");
 
-/////////// JOUER////////////
-game.addEventListener("click", function(){
-    jouer();
-    regle();
-    afficher();    
+//Jouer
+game.addEventListener("click", function () {
+  play();
+  rules();
+  score();
 });
 
-/////////// MANGER////////////
-eating.addEventListener("click", function(e){
-    manger();
-    regle();
-    afficher();    
+//Manger
+eating.addEventListener("click", function (e) {
+  eat();
+  rules();
+  score();
 });
 
-
-/////////// PROPRETÉE //////
-
-function poop(){
-
-    setInterval(function() {
-    var sal = document.getElementById("poop");
-    sal.style.display = "inline";
-    }, 6000);
+//Propreté
+function poop() {
+  setInterval(function () {
+    var relieve = document.getElementById("poop");
+    relieve.style.display = "inline";
+  }, 6000);
 }
 
-var sal = document.getElementById("poop");
-sal.onload = poop();
+var relieve = document.getElementById("poop");
+relieve.onload = poop();
 
-sal.addEventListener("click", function(){
-    sal.style.display = "none";
+relieve.addEventListener("click", function () {
+  relieve.style.display = "none";
 });
-
-
-
